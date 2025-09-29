475d2c07eb1706cd05b97d91c9f54b2197075b5c
Nilesh Sharma <nilesh.12th@gmail.com>
2025-09-29 22:31:22 +0530

    Music mat and scripts

---
+++ Music Box/Assets/Scripts/Bar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    AudioSource aud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("boop"))
+        {
+            aud.Play();
+        }
+    }
+}
